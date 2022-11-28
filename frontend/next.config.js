394a3b11a5457e9c36528c2dd0a40efcc3113966
Mostafa Kamal <hiremostafa@gmail.com>
2022-11-28 17:05:16 +0600

    set base uri

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  env: {
+    BASE_URI: 'http://127.0.0.1:5001'
+  }
 }
 
 module.exports = nextConfig
--- frontend/utils/http/axios.ts
+++ frontend/utils/http/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let http = axios.create({
-  baseURL: 'http://127.0.0.1:5001',
+  baseURL: process.env.BASE_URI || '',
   headers: {
     'Authorization': 'Bearer VCVRb99R4cCSw00Kv500HIKNIktDtKNTk5GkfNvF4rs'
   }

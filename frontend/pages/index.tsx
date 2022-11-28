8b8177de791570a26f9f0953302da168362e7264
Mostafa Kamal <hiremostafa@gmail.com>
2022-11-28 16:11:08 +0600

    refactor

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,75 +7,16 @@ import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
 import { TypeScriptLexer } from '@theGrammar/TypeScriptLexer';
 import { TypeScriptParser } from '@theGrammar/TypeScriptParser';
 import getAST from '@utils/ast-parser/get-ast';
-import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import Editor from "@monaco-editor/react";
+import exampleCodeData from '@constant/example-code';
 
 
 export default function Home() {
   const [code, setCode] = useState('' as any)
   const [theTree, setTheTree] = useState('' as any)
 
-  let theCode = `
-  import Reverse from "./Reverse.js";
-  
-  let SingleDigit = [1,2,3,4,5,8,9]
-  let DoubleDigit = [34,56,23]
-  
-  class AddSub {
-    constructor(num1,num2){
-      this.num1 = num1;
-      this.num2 = num2;
-    }
-    
-    addition() {
-      return this.num1 + this.num2;
-    }
-    
-    Substraction() {
-      return this.num1 + this.num2;
-    }
-  }
-  
-  function TDMatrix(num1,num2){
-    let arr = []
-    for(let i = 0; i < num1; i++){
-      let SubArr = []
-      for(let j = 0; j < num2; j++){
-        SubArr.push(j)
-      }
-      arr.push(SubArr)
-    }
-  }
-  
-  class MultDiv {
-    constructor(num1,num2){
-      this.num1 = num1;
-      this.num2 = num2;
-    }
-    
-    Multiplication() {
-      return this.num1 * this.num2;
-    }
-    
-    Division() {
-      return this.num1/this.num2;
-    }
-  }
-  
-  const MultipliedNum = new MultDiv(34,67)
-  console.log(MultipliedNum.Multiplication())
-  
-  function CallReverse(){
-    let val = Reverse(SingleDigit)
-    let addVal = new AddSub(34,55)
-    val.push(addVal.addition())
-    return val;
-  }
-  
-  const RevAs = CallReverse()
-  console.log(RevAs)
-      `
   useEffect(() => {
-    setCode(theCode)
+    setCode(exampleCodeData)
   }, [])
   
   useEffect(() => {
@@ -93,7 +34,6 @@ export default function Home() {
     let parser: any = new TypeScriptParser(tokenStream);
     parser.buildParseTree = true;
     let tree: any = parser.program()
-
     let ast: any = getAST(tree)
     setTheTree(ast)
   }
